168c0ab2e6957507f0b0f203b0968227124be616
Neposmart <scotty.is.taken@gmail.com>
2018-08-31 16:30:53 -0700

    Add sum logic, system exit 1 incorrect unit test

--- src/main/java/com/scottyrichard/calculator/CalculatorTest.java
+++ src/main/java/com/scottyrichard/calculator/CalculatorTest.java
@@ -5,10 +5,9 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
 	private Calculator calculator = new Calculator ();
 
+                System.exit(1);
 	@Test
 	public void testSum () {
 		assertEquals (5, calculator.sum (2,3));
-                System.exit(1);
 	}
-A
 }
