5edf28c083e548338622d5e237774115576834c5
Neposmart <scotty.is.taken@gmail.com>
2018-08-31 15:54:25 -0700

    Add sum logic, controller and unit test

---
+++ src/main/java/com/scottyrichard/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.scottyrichard.calculator;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+
+public class Calculator {
+
+     int sum(int a, int b) {
+
+          return a + b;
+
+     }
+
+}
